f9cbfe4c308d21816ace4a5160623af970e1967a
Theophile Thiberge <tthiberge@gmail.com>
2023-04-27 18:35:52 +0200

    trying further + solution

--- bracket-validator.rb
+++ bracket-validator.rb
@@ -10,25 +10,37 @@ p BRACKET_CLOSERS.include?('a')
 # p 'hello'.chars
 
 def check_brackets(string)
-  stack = []
+  stack_brackets = []
+  stack_other = []
+
   string.chars.each do |character|
     if BRACKET_OPENERS.include?(character)
-      stack << character
+      stack_brackets << character
 
     elsif BRACKET_CLOSERS.include?(character)
-      if stack.last == '(' && character == ')'
-        stack.pop
-      elsif stack.last == '[' && character == ']'
-        stack.pop
-      elsif stack.last == '{' && character == '}'
-        stack.pop
+      if stack_brackets.last == '(' && character == ')' || stack_brackets.last == '[' && character == ']' || stack_brackets.last == '{' && character == '}'
+        stack_brackets.pop
       else
-        puts "Sorry, but it is not closing properly ! Check again!"
+        "sorryyyy"
       end
+    else
+      stack_other << character
     end
-    puts "Everything checks! Congrats" if stack.empty?
   end
 end
 
-a = "Je ne crois pas non"
+# Solution jour 1 optionnel
+def valid_brackets?(string)
+  stack = []
+  symbols = { "{" => "}", "[" => "]", "(" => ")" }
+  string.each_char do |char|
+    return false if symbols.value?(char) && symbols.key(char) != stack.pop
+
+    stack << char if symbols.key?(char)
+  end
+  stack.empty?
+end
+
+a = "enivrant ()"
 p check_brackets(a)
+p valid_brackets?(a)
