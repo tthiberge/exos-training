5506e6897b44f81c73b2956e236d1432106bb064
Theophile Thiberge <tthiberge@gmail.com>
2023-05-19 20:29:47 +0200

    Succeeding in first solution option

--- cakeweekly_reversewords.rb
+++ cakeweekly_reversewords.rb
@@ -1,7 +1,16 @@
-
-
-
-
-
+def reverse_words(string)
+  output = []
+  string.split.each {|word| output.prepend(word)}
+  return output.join(' ')
+end
 
 string1 = "Hello my Love"
+
+3.times do
+  time1 = Time.now
+  reverse_words(string1)
+  time2 = Time.now
+
+  durée = time2 - time1
+  p "#{durée * 10**6} microseconds"
+end
