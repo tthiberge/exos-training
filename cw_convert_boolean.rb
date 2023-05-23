48e9237eea962029ed9289a61234c6792dc3070a
Theophile Thiberge <tthiberge@gmail.com>
2023-05-23 19:11:54 +0200

    Other variations

--- cw_convert_boolean.rb
+++ cw_convert_boolean.rb
@@ -1,5 +1,28 @@
 # Solution qui marche
+# C'est la best practice
 
 def bool_to_word bool
   bool ? "Yes" : "No"
+end
+
+
+# Variante explicite
+def bool_to_word bool
+  if bool == true
+    return 'Yes'
+  elsif bool == false
+    return 'No'
+  end
+end
+
+# Variante entre deux:
+def bool_to_word bool
+  bool == true ? 'Yes' : 'No'
+end
+
+
+# Variante avec hash - Interessant
+# Créer un hash et demander la valeur de la clé (crochet) faite du booléen
+def bool_to_word bool
+  {true => 'Yes', false => 'No'}[bool]
 end
