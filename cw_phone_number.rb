8db2f295e024c9a3eff078a36c907d5636005aa1
Theophile Thiberge <tthiberge@gmail.com>
2023-05-25 19:05:29 +0200

    Successful Kata phone number

---
+++ cw_phone_number.rb
@@ -0,0 +1,4 @@
+# Solution qui marche
+def create_phone_number(numbers)
+  return "(#{numbers[0]}#{numbers[1]}#{numbers[2]}) #{numbers[3]}#{numbers[4]}#{numbers[5]}-#{numbers[6]}#{numbers[7]}#{numbers[8]}#{numbers[9]}"
+end
