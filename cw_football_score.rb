8c48241741d16862747e36cfd909fb1edf27b9c7
Theophile Thiberge <tthiberge@gmail.com>
2023-05-27 18:30:39 +0200

    Successful kata - other variante

--- cw_football_score.rb
+++ cw_football_score.rb
@@ -12,3 +12,15 @@ def points(games)
   end
     return sum
 end
+
+# Autre variante à laquelle j'ai pensé pour moins faire if, else etc...
+# Fonctionne
+def points(games)
+  sum = 0
+  games.each do |score|
+    diff = score[0].to_i - score[-1].to_i
+    sum += 1 if diff.zero?
+    sum += 3 if diff.positive?
+  end
+  return sum
+end
