37907506b87618b645b59a92aaf046a6a1a6c297
Theophile Thiberge <tthiberge@gmail.com>
2023-05-27 17:41:08 +0200

    Variantes Kata

--- cw_find_unique.rb
+++ cw_find_unique.rb
@@ -3,3 +3,9 @@ def find_uniq(arr)
   arr.sort.first == arr.sort[1] ? arr.sort.last : arr.sort.first
 end
 
+# VARIANTES
+# On itère sur les élements constitutifs en ayant fait uniq et
+# ensuite on compte pour chacun de ces élements dans l'array initial
+def find_uniq(arr)
+  arr.uniq.each { |x| return x if arr.count(x) == 1 }
+end
