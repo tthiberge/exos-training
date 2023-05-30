f08251d1565b3a172bb122638dc235047cf4cb42
Theophile Thiberge <tthiberge@gmail.com>
2023-05-30 19:13:40 +0200

    Variantes

--- cw_sort_odd.rb
+++ cw_sort_odd.rb
@@ -1,3 +1,10 @@
+# You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+# Examples
+# [7, 1]  =>  [1, 7]
+# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
 # Fonctionne pas
 def sort_array(source_array)
   source_array.sort {|a, b| if a.odd? && b.odd? a <=> b}
@@ -20,3 +27,23 @@ def sort_array(source_array)
   end
   return output
 end
+
+
+# Variantes
+
+# La best practice c'est comme moi!! Deux améliorations:
+# J'aurais pu être plus rapide dans le select
+# le shift renvoie la valeur supprimée donc mes deux étapes peuvent se faire en une!
+def sort_array(source_array)
+  odds = source_array.select(&:odd?).sort
+  source_array.map { |n| n.even? ? n : odds.shift }
+end
+
+# Pas mal la fonction next plutôt que de les supprimer au fur et à mesure!
+def sort_array(xs)
+  odd = xs.select(&:odd?).sort.each
+  xs.map{ |x| x.odd? ? odd.next : x }
+end
+
+# En fait ma solution était comme il fallait faire, tout tourne autour de ça!!
+# Yessss
