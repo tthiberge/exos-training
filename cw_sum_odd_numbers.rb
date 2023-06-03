123357e600576c3e09cc36bebf1ed5ec843dd3f1
Theophile Thiberge <tthiberge@gmail.com>
2023-06-03 22:57:03 +0200

    Successful Kata sum odd numbers

---
+++ cw_sum_odd_numbers.rb
@@ -0,0 +1,12 @@
+Triangle des nombres impairs
+Calculer la somme des nombres sur la ligne name
+
+# Solution qui marche
+def row_sum_odd_numbers(n)
+  n**3
+end
+# En fait en calculant je me suis rendu compte que la somme était liée à la ligne.
+# Au début j'ai compris que c'était le carré de n, fois la ligne, donc en fait encore n!
+# donc n^3
+
+
