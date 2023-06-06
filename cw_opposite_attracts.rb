fe144be4da84f3b7a07a7d3cf42c11424bb92ffe
Theophile Thiberge <tthiberge@gmail.com>
2023-06-06 22:23:26 +0200

    Second successful proposition of mine

--- cw_opposite_attracts.rb
+++ cw_opposite_attracts.rb
@@ -1,3 +1,10 @@
+# Solution qui marche
 def lovefunc( flower1, flower2 )
   (flower1 % 2) != (flower2 % 2)
+end
+
+
+# Autre solution qui marche ?
+def lovefunc( flower1, flower2 )
+  flower1.odd? && flower2.even? || flower1.even? && flower2.odd?
 end
