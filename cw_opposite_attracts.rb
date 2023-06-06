112d253b467612c56ca97b6f0437afcf964947f6
Theophile Thiberge <tthiberge@gmail.com>
2023-06-06 22:53:38 +0200

    Variantes

--- cw_opposite_attracts.rb
+++ cw_opposite_attracts.rb
@@ -8,3 +8,16 @@ end
 def lovefunc( flower1, flower2 )
   flower1.odd? && flower2.even? || flower1.even? && flower2.odd?
 end
+
+# VARIANTES
+# Smart!!
+def lovefunc( flower1, flower2 )
+  (flower1 + flower2).odd?
+end
+
+# ça suffisait par rapport à moi qui aie fait les 2 cas car si c'est false == false ça fait true
+def lovefunc( flower1, flower2 )
+  flower1.odd? == flower2.even?
+end
+
+
